6d1963958c8434fb661d12d90691d86232f24510
nvll <krizs_098@outlook.com>
2025-02-18 11:31:35 -0400

    Script to oh my lord make it stop.

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - destroy betty 101
+*
+*Return: Always 0 (Suckcess)
+*/
+int main(void)
+{
+	char ay;
+
+	for (ay = 'a'; ay <= 'z'; ay++)
+		putchar(ay);
+
+	for (ay = 'A'; ay <= 'Z'; ay++)
+		putchar(ay);
+	putchar('\n')
+
+	return (0);
+}
