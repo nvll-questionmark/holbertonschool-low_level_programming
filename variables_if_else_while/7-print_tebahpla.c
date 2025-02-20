48307425d53225fca60e87098664f85a13059479
nvll <krizs_098@outlook.com>
2025-02-20 10:38:13 -0400

    Script to print alphabet in reverse.

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*Main - Entry Point, prints the alphabet in reverse.
+*
+*Return: Same as last time 0, Success.
+*/
+
+int main(void)
+{
+	char task7;
+
+	for (task7 = 122; task7 >= 97; task7--)
+		putchar(task7);
+
+	putchar('\n');
+	return (0);
+}
