2856e40eea78041259e1864277c9629334030bd9
nvll <krizs_098@outlook.com>
2025-02-20 10:08:36 -0400

    Script to print out base10 numbers.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry death
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base10 = 0;
+
+	for (base10 = 48; base10 < 58; base10++)
+		{
+		putchar(base10);
+		}
+	putchar('\n');
+	return (0);
+}
