2fb0d2381d9bc3024b9b37293220ad57f07271b7
nvll <krizs_098@outlook.com>
2025-02-20 10:17:06 -0400

    Script to do the same from previous task, no printf.

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Looks like I already recreated this task in the previous one.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int base10 = 0;
+
+	for (base10 = 48; base10 < 58; base10++)
+		{
+		putchar(base10);
+		}
+	putchar('\n');
+	return (0);
+}
