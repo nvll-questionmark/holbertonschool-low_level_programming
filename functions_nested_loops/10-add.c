70bee4520a2fd3444ea7b244a31ed7bc738b7196
nvll <krizs_098@outlook.com>
2025-02-23 19:52:42 -0400

    Program that add two ints && main.h, of course.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - sums two integers
+ *@slice1: int1
+ *@slice2: int2
+ *@slice3 - sum of them slices
+ *Return: sum of the integers
+ */
+
+int add(int slice1, int slice2)
+{
+	int slice3 = (slice1 + slice2);
+		return (slice3);
+}
