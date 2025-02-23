5026cc9bd54269e88ff081789a0cc6d995edaf06
nvll <krizs_098@outlook.com>
2025-02-23 19:30:43 -0400

    Program to print 9 times table && updtae main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+			if (y == 0)
+			{
+			_putchar(z + '0');
+			}
+
+			if (z < 10 && y != 0)
+			{
+				_putchar(44);
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(44);
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
