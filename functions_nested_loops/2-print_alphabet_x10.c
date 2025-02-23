9c27ba1bb7d27e5077b375635abc11980824f4af
nvll <krizs_098@outlook.com>
2025-02-23 12:54:21 -0400

    Program to write loop x10 && updated main.h

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out the alphabet x10.
+ * followed by a new line, of course.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alp;
+	int rep = 0;
+
+        while (rep++ < 10)
+	{
+		for (alp = 97; alp <= 122; alp++)
+                _putchar(alp);
+
+        	_putchar('\n');
+	}
+}
