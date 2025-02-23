aaec77e983f6843fb34e6d46b36bf391cae51d74
nvll <krizs_098@outlook.com>
2025-02-23 14:14:56 -0400

    Program to check for lowercase && updated main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: verified character
+ *
+ * Return: 1 if c is lowercase else 0 if otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 41 || c <= 90)
+		return (0);
+	else
+		return (1);
+}
