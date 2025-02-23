0c91ea9c1c5d0edffc77072b7ab7aef596ca69c4
nvll <krizs_098@outlook.com>
2025-02-23 13:00:15 -0400

    Bug sucks

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints out the alphabet x10.
- * followed by a new line, of course.
+ *print_alphabet_x10 - prints out the alphabet x10.
+ *followed by a new line, of course.
  */
 
 void print_alphabet_x10(void)
