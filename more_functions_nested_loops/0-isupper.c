3670de98f7469930305b6f59fcf575ec67a77549
nvll <krizs_098@outlook.com>
2025-02-25 13:37:44 -0400

    Fixed

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * isupper - verifies for uppercase characters
+ * _isupper - verifies for uppercase characters
  * @c: verified character
  * Return: 1 if c is upppercase, 0 if otherwise
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
