4bb84200db726c7abe5b20f13113b52aa9413ccb
nvll <krizs_098@outlook.com>
2025-02-25 14:06:02 -0400

    Function to multiple two ints.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: two integer
+ *
+ * Return: mul a.k.a result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
