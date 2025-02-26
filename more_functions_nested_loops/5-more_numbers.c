50bb9ee25841044088e6ec3088751cc05d1cd6f0
nvll <krizs_098@outlook.com>
2025-02-26 11:34:40 -0400

    Function that print from 0-14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times from 0-14
+ */
+
+void more_numbers(void)
+{
+	int x10 = 0;
+	int nl;
+
+	while (x10++ < 10)
+	{
+		for (nl = 0; nl < 15; nl++)
+		{
+			if (nl > 9)
+			_putchar((nl / 10) + '0');
+			_putchar((nl % 10)+ '0');
+		}
+	_putchar('\n');
+	}
+}
