4df2014dc0cf07ff167b6858f081cb4f42d9e9d1
nvll <krizs_098@outlook.com>
2025-02-26 18:58:59 -0400

    Function to print diagonaly omg

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line on the terminal
+ *@n: number of times '\' should be printed
+ */
+
+void print_diagonal(int n)
+{
+	char diag;
+
+	if (n > 0)
+	{
+		for (diag = 0; diag < n; diag++)
+		{
+			int spc = diag;
+
+			while (spc > 0)
+			{
+				_putchar(32);
+				spc--;
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
