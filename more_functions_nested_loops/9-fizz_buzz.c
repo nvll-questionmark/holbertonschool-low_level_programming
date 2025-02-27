9c1514ed8a5b3aaa4288e4806d85e43da60da6bc
nvll <krizs_098@outlook.com>
2025-02-26 20:01:35 -0400

    Ay

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,10 +8,14 @@
 int main(void)
 {
 	int l;
-	
+
 	for (l = 0; l <= 100; l++)
 	{
-		if (l % 3 == 0 && l % 5 ==0)
+		if (l == 1)
+		{
+			printf("%d", l);
+		}
+		else if (l % 3 == 0 && l % 5 ==0)
 		{
 			printf(" Fizzbuzz");
 		}
