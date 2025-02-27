8a67000e2dcf63968f95994495327e12abb748d7
nvll <krizs_098@outlook.com>
2025-02-26 20:24:21 -0400

    Function to print triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size again ;-;
+ */
+
+void print_triangle(int size)
+{
+	int tr;
+	int angle;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (tr = 1; tr <= size; tr++)
+		{
+			for (angle = size - tr; angle > 0; angle--)
+			{
+				_putchar(32);
+			}
+			for (angle = 1; angle <= tr; angle++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
