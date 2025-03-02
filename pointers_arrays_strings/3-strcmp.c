e3ec5099c71f32b39ee3d33282781eb870a2fb46
nvll <krizs_098@outlook.com>
2025-03-02 13:28:51 -0400

    Uh

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,11 @@
  * Return: 0 if s1=s2, more than 0 if s1>s2, less than 0 if s1>s2
  */
 
-char _strcmp(char *s1, char s2)
+int _strcmp(char *s1, char s2)
 {
 	while (*s1 == *s2)
 	{
-		if (s1 == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
