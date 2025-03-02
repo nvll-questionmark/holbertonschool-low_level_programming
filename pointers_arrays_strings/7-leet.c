e17a49e7ce4504e7c62fbbfcca77e0ea5353cd00
nvll <krizs_098@outlook.com>
2025-03-02 14:23:33 -0400

    funtion to encode in 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes in 1337
+ * @s: encoded value
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int p;
+	int q;
+
+	char *n1 = "aAeEoOtT1L";
+	char *n2 = "4433007711";
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		for (q = 0; q < 10; q++)
+		{
+			if s[p] == n1[q]}
+			{
+				s[p] = n2[q];
+			}
+		}
+	}
+
+	return (s);
+}
