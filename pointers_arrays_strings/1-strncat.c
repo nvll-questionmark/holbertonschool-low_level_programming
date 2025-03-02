e397b0bf1a9b1e739bfb762b276ba78c4795a5c3
nvll <krizs_098@outlook.com>
2025-03-02 12:45:40 -0400

    function to concatenate

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate more
+ * @dest: pointa
+ * @src: pointa
+ * @n: given value
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int str1;
+	int str2;
+
+	for (str1 = 0; dest[str1]; str1++)
+		;
+	for (str2 = 0; src[str2] && str2 < n; str2++)
+	{
+		dest[str1] = src[str2];
+		str1++;
+	}
+	return (dest);
+}
