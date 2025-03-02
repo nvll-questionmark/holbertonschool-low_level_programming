b37f7d0d046676188df64e12ec1dfb019ceaf464
nvll <krizs_098@outlook.com>
2025-03-02 14:41:19 -0400

    I

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,21 +9,22 @@
 
 char *leet(char *s)
 {
-        int p;
-        int q;
+	int p;
+	int q;
 
-        char n1[] = "aAeEoOtT1L";
-        char n2[] = "4433007711";
+	char *n1 = "aAeEoOtTlL";
+	char *n2 = "4433007711";
 
-        for (p = 0; s[p] != '\0'; p++)
-        {
-                for (q = 0; q < 10; q++)
-                {
-                        if (s[p] == n1[q])
-                        {
-                                s[p] = n2[q];
-                        }
-                }
-        }
-        return (s);
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		for (q = 0; q < 10; q++)
+		{
+			if (s[p] == n1[q])
+			{
+				s[p] = n2[q];
+			}
+		}
+	}
+
+	return (s);
 }
