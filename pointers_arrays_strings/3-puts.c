2f23d1ea175f8823a8a5200a9f8fd42d001f7ff2
nvll <krizs_098@outlook.com>
2025-03-02 09:10:55 -0400

    Function to print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - funtion to print a string
+ *@str: pointer to the string
+ */
+
+void _puts(char *str)
+{
+	if (*str)
+		printf("%s\n", str);
+	
+}
