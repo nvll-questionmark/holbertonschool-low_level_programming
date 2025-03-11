d98c6ad7b3d04a8f12bfebe58328b378b698d214
nvll <krizs_098@outlook.com>
2025-03-11 07:36:51 -0400

    Rev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to print a string in reverse
+ * @s: uffff thing given
+ */
+
+void _print_rev_recursion(char *S)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
