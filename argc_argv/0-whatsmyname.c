a51fda773549a59a2d8cb924732609b95a73a36a
nvll <krizs_098@outlook.com>
2025-03-11 10:14:13 -0400

    myname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - name of program
+ * @argc: number of argument
+ * @argv: array of arguments
+ *
+ * Return: 0 Succes G
+ */
+
+in main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
