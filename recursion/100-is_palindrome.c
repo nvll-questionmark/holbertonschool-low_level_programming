aa06cc096830e815e20a1140697840f20d6d89e5
nvll <krizs_098@outlook.com>
2025-03-11 08:50:15 -0400

    palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int ver_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: given variable
+ *
+ * Return: 1 is yes, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns leng of str
+ * @s: str
+ *
+ * Return: len
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * ver_pal - verifies the chars recursively for palindrome
+ * @s: string
+ * @i: iterate
+ * @len: length
+ *
+ * Return: 1 if pal, 0 if not
+ */
+
+int ver_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (ver_pal(s, i + 1, len - 1));
+}
