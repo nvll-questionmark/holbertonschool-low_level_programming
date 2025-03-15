c3e5dd270fe9a2d22fdd641c87c9416f14ba8fc9
nvll <krizs_098@outlook.com>
2025-03-15 08:36:00 -0400

    Function to make array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - function to make an array an initialize it
+ *
+ * @size: size of array
+ * @c: char to initialize the array
+ *
+ * Return: pointer to the array, NULL if error
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; p[i] == '\0'; ++i)
+		p[i] = c;
+
+	return (p);
+}
