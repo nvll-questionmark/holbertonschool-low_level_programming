8faac17c0dde6d9cf3a7f2c0f96e1331c002e6fd
nvll <krizs_098@outlook.com>
2025-03-15 10:38:30 -0400

    funtion to allocate mem in arr

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates mem for a arr
+ * @nmemb: number of elements
+ * @size: size in bytes
+ *
+ * Return: pointa to the alloc mem, NULL if fails
+ */
+
+void *_calloc(unsigned in nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int bytes;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	bytes = nmemb * size;
+	p = malloc(bytes);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < bytes; ++i)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
