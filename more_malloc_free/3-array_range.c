f7e773b9e30e41a0f42f14cb9a67597136a1f43a
nvll <krizs_098@outlook.com>
2025-03-15 10:40:50 -0400

    P

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - creates array of ints
+ * @min: minimum rnage
+ * @max: maximun range
+ *
+ * Return: pointa
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+	int size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	p = malloc(sizeof(int) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; ++i)
+		p[i] = ++min;
+
+	return (p);
+}
