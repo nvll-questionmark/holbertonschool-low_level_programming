5dd3c1fb183bd856a8807fd99c5db39499effac7
nvll <krizs_098@outlook.com>
2025-03-15 09:21:33 -0400

    Malloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - function that return pointer to two dimensional array of ints
+ * @width: matrix width
+ * @height: matric height
+ *
+ * Return: Supa pointa
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i;
+	int j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	p = (int **) malloc(sizeof(int *) * height);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; ++i)
+	{
+		p[i] = (int *) malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{
+			free(p);
+			for (j = 0; j <= 1; ++j)
+				free(p[j]);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; ++i)
+	{
+		for (j = 0; j < width; j ++j)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
